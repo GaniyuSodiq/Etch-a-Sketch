cd4e5baaa5a909c5822620ae969a81033ccea358
Sodiq Olaniyi <82044466+GaniyuSodiq@users.noreply.github.com>
2025-03-27 16:55:06 +0100

    Set background to the divs upon mouse movement
    This is the main function of the project

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let containerBox = document.querySelector("#container");
 for (let i =1; i <= 16; i++){
     for (let j = 1; j <= 16; j++){
         const divBox = document.createElement("div");
+        divBox.addEventListener("mouseover", () => {
+            divBox.style.backgroundColor = "lightgreen"
+        })
         containerBox.appendChild(divBox);
     }
 }
