7b61483d4be6bf85e3663231afe9889b0a97a834
Sodiq Olaniyi <82044466+GaniyuSodiq@users.noreply.github.com>
2025-03-27 17:41:33 +0100

    Trully research is needed
    I did a lot of research and created test files and mini project
    I used reserch from Stackoverflow to practice in this mini projects and
    files.
    I finally got an answer that i need to study in order to understand
    And be a ble to replicate on my own.
    RESEARCH AND SETTING UP SIDE MINI PROJECT BASED ON THE ACTUAL PROJECT'S
    RESEARCH IS AMAZIN AND EYE OPENING.
    you learn from experts in the field

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let containerBox = document.querySelector("#container");
+// let containerBox = document.querySelector("#container");
 
-for (let i =1; i <= 16; i++){
-    for (let j = 1; j <= 16; j++){
-        const divBox = document.createElement("div");
-        divBox.addEventListener("mouseover", () => {
-            divBox.style.backgroundColor = "lightgreen"
+// for (let i =1; i <= 16; i++){
+//     for (let j = 1; j <= 16; j++){
+//         const divBox = document.createElement("div");
+//         divBox.addEventListener("mouseover", () => {
+//             divBox.style.backgroundColor = "lightgreen"
+//         })
+//         containerBox.appendChild(divBox);
+//     }
+// }
+
+
+function makeGrids(size) {
+    let screen = document.querySelector("#container");
+    for (let i = 0; i < size; i++) {
+      let column = document.createElement("div");
+        // column.addEventListener("mouseover", () => {
+        //     column.style.backgroundColor = "lightgreen"
+        // })
+    //   column.classList.add("column");
+      for (let j = 1; j <= size; j++) {
+        let row = document.createElement("div");
+        row.addEventListener("mouseover", () => {
+            row.style.backgroundColor = "lightgreen"
         })
-        containerBox.appendChild(divBox);
+        // row.classList.add("row");
+        row.style.border = "2px solid black";
+        row.innerText = (i * size) + j;
+        column.appendChild(row);
+      }
+      screen.appendChild(column);
     }
-}
-
+  }
+  
+  makeGrids(16);
