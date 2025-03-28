bed20b12bd6709e019735224b79bcd77036ee62e
Sodiq Olaniyi <82044466+GaniyuSodiq@users.noreply.github.com>
2025-03-28 22:04:37 +0100

    Make the color random
    This makes the user experience beautiful while hovering over the div

--- script.js
+++ script.js
@@ -2,26 +2,18 @@ const containerFirst = document.querySelector(".containerFirst");
 const gridRequestBtn = document.querySelector("#gridRequestBtn");
 const removeDiv = document.querySelector("#removeDiv");
 
-// THIS IS NEVER NEEDED 
-// let clearGridBtn = document.createElement("button");
-// clearGridBtn.textContent = "Clear Grid";
-// removeDiv.appendChild(clearGridBtn);
-
-// clearGridBtn.addEventListener("click", () => {
-//   // screen.remove();
-// })
-
+// THE LOADED GRID
 let screen = document.createElement("div");
 screen.setAttribute('id', 'container');
-
-// THE LOADED GRID
 for (let i = 0; i < 16; i++) {
   let column = document.createElement("div");
   column.classList.add("column");
   for (let j = 1; j <= 16; j++) {
     let row = document.createElement("div");
     row.addEventListener("mouseover", () => {
-        row.style.backgroundColor = "lightgreen"
+      let randomColor = 
+      `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+      row.style.backgroundColor = randomColor;
     })
     row.classList.add("row");
     row.style.border = "1px solid red";
@@ -33,15 +25,7 @@ containerFirst.appendChild(screen);
 
 // THE SUBSEQUENT COMPUTED GRID
 function makeGrid(size) {
-  if (screen != "") {
-    // I WAS TRYING TO CALL THIS FROM AN EXTERNAL FUNCTION - ERROR
-    // FIGURED ANYTHING THAT WILL REMOVE ANY PART OF THE DOM SHOULD BE CREATED WIT THE PART OF D DOM
-    // KUST LIKE CREATING A TODO LIST
-    screen.remove();
-  }
-  // This part made me suffer really hard
-  // hmmm 2 whole days of strugling 
-  // - i didnt know i had to create the div container each time the grid is ran.
+  if (screen != "") {  screen.remove();  };
   screen = document.createElement("div");
   screen.setAttribute('id', 'container');
 
@@ -51,7 +35,9 @@ function makeGrid(size) {
     for (let j = 1; j <= size; j++) {
       let row = document.createElement("div");
       row.addEventListener("mouseover", () => {
-          row.style.backgroundColor = "lightgreen"
+        let randomColor = 
+        `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)})`;
+        row.style.backgroundColor = randomColor;
       })
       row.classList.add("row");
       row.style.border = "1px solid red";
@@ -62,16 +48,9 @@ function makeGrid(size) {
   containerFirst.appendChild(screen);
 }
 
-
 gridRequestBtn.addEventListener("click", () => {
-  let userGivenSize = prompt();
+  let userGivenSize = prompt(`Type a number between 1-100 for a Grid layout`, "");
   makeGrid(userGivenSize);
 })
-
-
-
-//   listItemBtn.addEventListener("click", () => {
-//     listItem.remove();
-// })
 
 
