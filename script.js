9021f92ca5aac4a5592b351e1220022b09bd5d3f
Sodiq Olaniyi <82044466+GaniyuSodiq@users.noreply.github.com>
2025-03-28 18:49:10 +0100

    THIS IS A MILESTONE IN MY STRUGLLING
    This discovery seemed simple to me but i struggled for 2 days
    I finally did it but was hard getting to the right code
    couldnt find a suitable answer online
    I had to understand each error message, each empty screen, went thru the
    code over and over to understand where the present mistake came from

--- script.js
+++ script.js
@@ -1,36 +1,77 @@
-// let containerBox = document.querySelector("#container");
+const containerFirst = document.querySelector(".containerFirst");
+const gridRequestBtn = document.querySelector("#gridRequestBtn");
+const removeDiv = document.querySelector("#removeDiv");
 
-// for (let i =1; i <= 16; i++){
-//     for (let j = 1; j <= 16; j++){
-//         const divBox = document.createElement("div");
-//         divBox.addEventListener("mouseover", () => {
-//             divBox.style.backgroundColor = "lightgreen"
-//         })
-//         containerBox.appendChild(divBox);
-//     }
-// }
+// THIS IS NEVER NEEDED 
+// let clearGridBtn = document.createElement("button");
+// clearGridBtn.textContent = "Clear Grid";
+// removeDiv.appendChild(clearGridBtn);
 
+// clearGridBtn.addEventListener("click", () => {
+//   // screen.remove();
+// })
 
-function makeGrids(size) {
-    let screen = document.querySelector("#container");
-    for (let i = 0; i < size; i++) {
-      let column = document.createElement("div");
-        // column.addEventListener("mouseover", () => {
-        //     column.style.backgroundColor = "lightgreen"
-        // })
-    //   column.classList.add("column");
-      for (let j = 1; j <= size; j++) {
-        let row = document.createElement("div");
-        row.addEventListener("mouseover", () => {
-            row.style.backgroundColor = "lightgreen"
-        })
-        // row.classList.add("row");
-        row.style.border = "2px solid black";
-        row.innerText = (i * size) + j;
-        column.appendChild(row);
-      }
-      screen.appendChild(column);
-    }
+let screen = document.createElement("div");
+screen.setAttribute('id', 'container');
+
+// THE LOADED GRID
+for (let i = 0; i < 16; i++) {
+  let column = document.createElement("div");
+  column.classList.add("column");
+  for (let j = 1; j <= 16; j++) {
+    let row = document.createElement("div");
+    row.addEventListener("mouseover", () => {
+        row.style.backgroundColor = "lightgreen"
+    })
+    row.classList.add("row");
+    row.style.border = "1px solid red";
+    column.appendChild(row);
   }
-  
-  makeGrids(16);
+  screen.appendChild(column);
+}
+containerFirst.appendChild(screen);
+
+// THE SUBSEQUENT COMPUTED GRID
+function makeGrid(size) {
+  if (screen != "") {
+    // I WAS TRYING TO CALL THIS FROM AN EXTERNAL FUNCTION - ERROR
+    // FIGURED ANYTHING THAT WILL REMOVE ANY PART OF THE DOM SHOULD BE CREATED WIT THE PART OF D DOM
+    // KUST LIKE CREATING A TODO LIST
+    screen.remove();
+  }
+  // This part made me suffer really hard
+  // hmmm 2 whole days of strugling 
+  // - i didnt know i had to create the div container each time the grid is ran.
+  screen = document.createElement("div");
+  screen.setAttribute('id', 'container');
+
+  for (let i = 0; i < size; i++) {
+    let column = document.createElement("div");
+    column.classList.add("column");
+    for (let j = 1; j <= size; j++) {
+      let row = document.createElement("div");
+      row.addEventListener("mouseover", () => {
+          row.style.backgroundColor = "lightgreen"
+      })
+      row.classList.add("row");
+      row.style.border = "1px solid red";
+      column.appendChild(row);
+    }
+    screen.appendChild(column);
+  }
+  containerFirst.appendChild(screen);
+}
+
+
+gridRequestBtn.addEventListener("click", () => {
+  let userGivenSize = prompt();
+  makeGrid(userGivenSize);
+})
+
+
+
+//   listItemBtn.addEventListener("click", () => {
+//     listItem.remove();
+// })
+
+
